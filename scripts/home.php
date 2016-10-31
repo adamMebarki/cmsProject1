2bf6ad5869d4120a231847abc3aee2b4bb525c4c
1606149 <mebaad10@gmail.com>
2016-10-31 14:00:29 +0000

    First test with home page

--- scripts/home.php
+++ scripts/home.php
@@ -1,10 +1,10 @@
 <?php
-include ("scripts/tinymce/header.php");
+include ("scripts/header.php");
 echo "
 <main>
 <p>Welcome to the blog of Charles Babbage! In this blog you will
 see all of my insights and wonderful things</p>
 </main>
 ";
-include ("scripts/tinymce/footer.php");
+include ("scripts/footer.php");
 ?>
