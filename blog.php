511d67696ce37ea6518e6b962ee8bdfedd44096b
1606149 <mebaad10@gmail.com>
2016-10-31 14:21:50 +0000

    First test with home page

---
+++ blog.php
@@ -0,0 +1,12 @@
+<?php
+    include("scripts/dbconnect.php");
+    include("scripts/header.php");
+
+    echo "
+        <main>
+        <h2>Blog Article</h2>
+        <p>Below is a list of all blog articles</p>
+        <ul>
+    "
+
+?>
