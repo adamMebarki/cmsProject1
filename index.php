19a8866d90db1a2d4e3657fd5b219a7ba2003594
1606149 <mebaad10@gmail.com>
2016-10-31 15:57:42 +0000

    First test with home page

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $rules = array(
 //
 //main pages
 //
-    'about' => "/about.php",
+    'about.php' => "/about",
     'contactus' => "/contactus",
     'blog' => "/blog",
     'blog_article' => "/blog/(?'blogID'[\w\-]+)",
